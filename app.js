5da75284778638556f3e16523abbd4e52912bda5
Luigi Cornacchia <99611982+luigicornacchia@users.noreply.github.com>
2026-06-03 15:10:19 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -87,19 +87,21 @@ const MATCHES = [
   { id: "m11", date: "4 Giugno", time: "21:00", home: "FC Orsa Maggiore", away: "Mirabellarum Robur", field: "Campo", group: "A" },
 
   // 6 Giugno Sabato
-  { id: "m12", date: "6 Giugno", time: "19:00", home: "Bar in Piazza", away: "RP Gold Team", field: "Campo", group: "B" },
-  { id: "m13", date: "6 Giugno", time: "20:00", home: "RP Team", away: "Team DR", field: "Campo", group: "A" },
-  { id: "m14", date: "6 Giugno", time: "21:00", home: "Monteleone", away: "FC Orsa Maggiore", field: "Campo", group: "A" },
+  { id: "m12", date: "6 Giugno", time: "20:00", home: "RP Team", away: "Team DR", field: "Campo", group: "A" },
+  { id: "m13", date: "6 Giugno", time: "21:00", home: "Monteleone", away: "FC Orsa Maggiore", field: "Campo", group: "A" },
 
   // 8 Giugno Lunedi
-  { id: "m15", date: "8 Giugno", time: "19:00", home: "Zetaquadro Bar", away: "Red Wolves", field: "Campo", group: "B" },
-  { id: "m16", date: "8 Giugno", time: "20:00", home: "Monteleone", away: "Mirabellarum Robur", field: "Campo", group: "A" },
-  { id: "m17", date: "8 Giugno", time: "21:00", home: "RP Gold Team", away: "HP", field: "Campo", group: "B" },
+  { id: "m14", date: "8 Giugno", time: "20:00", home: "Monteleone", away: "Mirabellarum Robur", field: "Campo", group: "A" },
+  { id: "m15", date: "8 Giugno", time: "21:00", home: "RP Gold Team", away: "HP", field: "Campo", group: "B" },
 
   // 10 Giugno Mercoledì
-  { id: "m18", date: "10 Giugno", time: "19:00", home: "RP Team", away: "Mirabellarum Robur", field: "Campo", group: "A" },
-  { id: "m19", date: "10 Giugno", time: "20:00", home: "Zetaquadro Bar", away: "Bar in Piazza", field: "Campo", group: "B" },
-  { id: "m20", date: "10 Giugno", time: "21:00", home: "Red Wolves", away: "RP Gold Team", field: "Campo", group: "B" },
+  { id: "m16", date: "10 Giugno", time: "19:00", home: "RP Team", away: "Mirabellarum Robur", field: "Campo", group: "A" },
+  { id: "m17", date: "10 Giugno", time: "20:00", home: "Zetaquadro Bar", away: "Bar in Piazza", field: "Campo", group: "B" },
+  { id: "m18", date: "10 Giugno", time: "21:00", home: "Red Wolves", away: "RP Gold Team", field: "Campo", group: "B" },
+
+  // 12 Giugno Venerdì
+  { id: "m19", date: "12 Giugno", time: "20:00", home: "Bar in Piazza", away: "RP Gold Team", field: "Campo", group: "B" },
+  { id: "m20", date: "12 Giugno", time: "21:00", home: "Zetaquadro Bar", away: "Red Wolves", field: "Campo", group: "B" },
 ];
 
 function initializeFirebase() {
